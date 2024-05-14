20e576dd7b14ee3e385800998ce37e3c4f4dcb8a
abhisheksingh <abhisheksingh.geek123@gmail.com>
2024-05-15 03:18:35 +0530

    Leaders in an Array in Order

--- src/Arrays/LeaderInAnArray.java
+++ src/Arrays/LeaderInAnArray.java
@@ -2,6 +2,8 @@ package Arrays;
 
 //Find Leader by finding suffix maximum
 
+import java.util.Stack;
+
 /*
 The idea is to scan all the elements from right to left in an array and keep track of the maximum till now.
 When the maximum changes its value, print it.
@@ -10,8 +12,11 @@ public class LeaderInAnArray {
     public static void main(String[] args) {
         int[] inputArr = {16,17,4,3,5,2};
         printLeaders(inputArr);
+        printLeadersInOrder(inputArr);
     }
-
+    /*
+    Order is not restored while priting leaders
+     */
     public static void printLeaders(int[] arr){
         int max = arr[arr.length-1];
         System.out.println(max);
@@ -20,6 +25,21 @@ public class LeaderInAnArray {
                 max = arr[i];
                 System.out.println(max);
             }
+        }
+    }
+
+    public static void printLeadersInOrder(int[] arr){
+        int length = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[length-1]);
+        for(int i=length-2; i>=0; i--){
+            if(arr[i] >= stack.peek()){
+                stack.push(arr[i]);
+            }
+        }
+        System.out.println("Leaders in Order:");
+        while(!stack.empty()){
+            System.out.print(stack.pop()+ " ");
         }
     }
 }
