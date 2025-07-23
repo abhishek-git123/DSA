4895b7027c77341a864fce4067247b8c1e8ae63c
abhisheksingh <abhisheksingh.geek123@gmail.com>
2025-07-24 02:38:17 +0530

    Print all the divisors of a number and prevent duplicate, but not in sorted order

--- src/Mathematics/PrintAllDivisors.java
+++ src/Mathematics/PrintAllDivisors.java
@@ -3,18 +3,22 @@ package Mathematics;
 //It prints all the divisors of a number, but not in sorted order
 public class PrintAllDivisors {
     public static void main(String[] args) {
-        int n= 30;
+        int n= 25;
         printAllDivisors(n);
     }
 
     private static void printAllDivisors(int n){
         System.out.print(1+" "+n+ " ");
 
-        for(int i=2; i*i <n ;i++){
+        for(int i=2; i*i <=n ;i++){
             if(n%i == 0){
                 System.out.print(i+ " ");
-                n = n/i;
-                System.out.print(n+ " ");
+                //in case of perfect squares like 25, when i becomes 5, it will print 5, two times.
+                //So to avoid duplicate printing of i, this condition is applied
+                if(i != n/i){
+                    System.out.print(n/i + " ");
+                }
+
             }
         }
     }
